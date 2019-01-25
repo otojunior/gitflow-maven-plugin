ae54fa2bad92dfab626a90df101baf6f5a063023
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2019-01-25 14:01:17 -0200

    Commit inicial

---
+++ src/main/java/br/org/otojunior/plugin/gitflow/SampleMojo.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.org.otojunior.plugin.gitflow;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * @author Oto Soares Coelho Junior (oto.coelho-junior@serpro.gov.br)
+ *
+ */
+@Mojo(name = "sample")
+public class SampleMojo extends AbstractMojo {
+	/**
+	 * 
+	 */
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("Sample Mojo: gitflow plugin");
+	}
+}
